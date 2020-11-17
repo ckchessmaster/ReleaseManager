d16447a69c78b217e607f062f9bf498862bb045c
Chris Kingdon <ckchessmaster@gmail.com>
2020-11-17 12:27:26 -0500

    misc stuff

--- web-src/tests/unit/releaseService.spec.js
+++ web-src/tests/unit/releaseService.spec.js
@@ -1,8 +1,8 @@
 import { shallowMount } from '@vue/test-utils'
 
 describe('Comparing version numbers', () => {
-  it('renders props.msg when passed', () => {
+  // it('renders props.msg when passed', () => {
 
-    expect(true).toMatch(true)
-  })
+  //   expect(true).toMatch(true)
+  // })
 })
